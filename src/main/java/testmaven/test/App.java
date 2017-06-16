5c45d7877df80dcdb625b27e396920c9912365eb
wesbilel <bilel.oueslati@esprit.tn>
2017-06-16 11:22:15 +0100

    jjj

--- src/main/java/testmaven/test/App.java
+++ src/main/java/testmaven/test/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!!!!!hhh!" );
+        System.out.println( "Hello Worldddd!!!!!hhh!" );
     }
 }
